13f3280cc6d71bd93e3fe2e3e670dee58c7c341a
Déborah <deborah.bencteux@hotmail.com>
2023-04-26 10:49:41 +0200

    personnes

--- exoGit/src/main/java/exoGit/Personnes.java
+++ exoGit/src/main/java/exoGit/Personnes.java
@@ -2,4 +2,26 @@ package exoGit;
 
 public class Personnes {
 
+	String nom;
+	String prenom;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	@Override
+	public String toString() {
+		return "Personnes [nom=" + nom + ", prenom=" + prenom + "]";
+	}
+	
+	
 }
