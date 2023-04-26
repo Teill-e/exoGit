2dfd777e238f929f60406ab2fb168067a9b4ccc7
olivier gozlan <ybrahimaelhafid@gmail.com>
2023-04-26 11:28:35 +0200

    test

---
+++ exoGit/src/main/java/test/App.java
@@ -0,0 +1,15 @@
+package test;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Personne personne1 = new personne;
+		Personne personne2 = new personne;
+		Entreprise entreprise = new entreprise; 
+	    
+	}
+
+}
+
+
